1adb7c09668ade6a42a5d2124087f21d4ae64bba
darkned <darkned.23@gmail.com>
2023-03-03 21:06:27 -0500

    Cambios de escenas implementados

---
+++ Assets/Edward/Scripts/EventSystem.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+public class EventSystem : MonoBehaviour
+{
+    //Metodo para cargar la misma escena
+    public void ResetScene()
+    {
+        SceneManager.LoadScene(sceneBuildIndex: 0);
+    }
+    //Metodo para cargar la siguiente escena
+    public void NextScene()
+    {
+        SceneManager.LoadScene(sceneBuildIndex: +1);
+    }
+    //Metodo para cargar la anterior escena
+    public void ReturnScene()
+    {
+        SceneManager.LoadScene(sceneBuildIndex: -1);
+    }
+    //Metodo para cerrar el juego
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}

90758029b71af58a0f0f64e8ba761be49b963800
darkned <darkned.23@gmail.com>
2023-03-05 02:36:01 -0500

    Movimiento, salto y rotacion del Player terminado

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,37 +1,64 @@
-using System;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private float jumpForce = 200f, forceMove = 100f;
-    private Rigidbody _rigidbody;
-    private PlayerInput _playerInput;
-    private Vector2 input;
+    [SerializeField] private float speedMove = 10f;
+    [SerializeField] private Transform cam;
+    
+    [SerializeField] private float jumpForce = 10f;
+    [SerializeField] private float gravity = -9.8f;
+    [SerializeField] private float groundDistance = 0.3f;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundMask;
+    
+    private CharacterController _characterController;
+    private float turnSmoothTime = 0.1f;
+    private float turnSmoothVelocity;
+    
+    private Vector3 velocity;
+    private bool isGrounded;
     private void Start()
     {
-        _rigidbody = GetComponent<Rigidbody>();
-        _playerInput = GetComponent<PlayerInput>();
+        _characterController = GetComponent<CharacterController>();
         Cursor.lockState = CursorLockMode.Locked;
     }
+    
     private void Update()
     {
-        input = _playerInput.actions["Move"].ReadValue<Vector2>();
-        //rotacion camara
-        float targetAngle = Mathf.Atan2(input.x, input.y) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
+        Jump();
+        Move();
     }
-    private void FixedUpdate()
+    
+    private void Move()
     {
-        _rigidbody.AddForce(new Vector3(input.x, 0f, input.y) * forceMove);
-    }
-    //Metodo para saltar
-    public void Jump(InputAction.CallbackContext callbackContext)
-    {
-        if(callbackContext.performed)
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+        
+        if (direction.magnitude >= 0.1f)
         {
-            _rigidbody.AddForce(Vector3.up * jumpForce);
-            Debug.Log("Jumping...");
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+            _characterController.Move(moveDir * (speedMove * Time.deltaTime));
         }
+    }
+
+    private void Jump()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y -= 2f;
+        }
+        
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpForce * -2 * gravity);
+        }
+        
+        velocity.y += gravity * Time.deltaTime;
+        _characterController.Move(velocity * Time.deltaTime);
     }
 }
